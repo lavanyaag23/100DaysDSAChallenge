b9d4c37f8524aae0e6d8c42bcf13d2bd3e254a61
Lavanya Agrawal <lavanya.kanak.agrawal@gmail.com>
2026-07-07 11:58:17 +0530

    Rename day folders to two-digit format

--- Day_01/Question1.java
+++ Day_01/Question1.java
@@ -1,4 +1,4 @@
-/*Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+/*Problem: Write a program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
 
 Input:
 - First line: integer n
