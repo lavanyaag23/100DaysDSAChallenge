0bdf1e8566944375e9a850d8d348728a3907272d
Lavanya Agrawal <lavanya.kanak.agrawal@gmail.com>
2026-07-13 12:38:35 +0530

    Day 17: implement max and min element search in array using Java

---
+++ Day_17/Question1.java
@@ -0,0 +1,49 @@
+/* Problem: Write a program to find the maximum and minimum values present in a given array of integers.
+
+Input:
+- First line: integer n
+- Second line: n integers
+
+Output:
+- Print the maximum and minimum elements
+
+Example:
+Input:
+6
+3 5 1 9 2 8
+
+Output:
+Max: 9
+Min: 1*/
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+
+        sc.close();
+    }
+}
