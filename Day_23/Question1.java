82991613fa33d1d8104a9b1dcb5e582c1d47c2db
Lavanya Agrawal <lavanya.kanak.agrawal@gmail.com>
2026-07-19 08:53:54 +0530

    Day 23: implement merge two sorted linked lists in Java

---
+++ Day_23/Question1.java
@@ -0,0 +1,111 @@
+/* Problem: Merge Two Sorted Linked Lists - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (first list)
+- Third line: integer m
+- Fourth line: m space-separated integers (second list)
+
+Output:
+- Print the merged linked list elements, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+4
+15 25 35 45
+
+Output:
+10 15 20 25 30 35 40 45 50
+
+Explanation:
+Compare nodes of both lists, append smaller to result, continue until all nodes are merged. */
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Question1 {
+
+    // Insert node at the end
+    static Node insert(Node head, int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            return newNode;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        return head;
+    }
+
+    // Merge two sorted linked lists
+    static Node mergeLists(Node head1, Node head2) {
+        Node dummy = new Node(0);
+        Node tail = dummy;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (head1 != null) {
+            tail.next = head1;
+        } else {
+            tail.next = head2;
+        }
+
+        return dummy.next;
+    }
+
+    // Print linked list
+    static void printList(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Node head1 = null;
+
+        for (int i = 0; i < n; i++) {
+            head1 = insert(head1, sc.nextInt());
+        }
+
+        int m = sc.nextInt();
+        Node head2 = null;
+
+        for (int i = 0; i < m; i++) {
+            head2 = insert(head2, sc.nextInt());
+        }
+
+        Node mergedHead = mergeLists(head1, head2);
+        printList(mergedHead);
+
+        sc.close();
+    }
+}
