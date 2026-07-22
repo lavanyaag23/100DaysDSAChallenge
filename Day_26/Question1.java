1e67208fde2fbd5d6072fc68251b92c1497f778e
Lavanya Agrawal <lavanya.kanak.agrawal@gmail.com>
2026-07-22 14:24:51 +0530

    Day 26: implement doubly linked list insertion and traversal

---
+++ Day_26/Question1.java
@@ -0,0 +1,80 @@
+/* Problem: Doubly Linked List Insertion and Traversal - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the linked list elements in forward order, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+
+Explanation:
+Each node has data, next, prev. Insert nodes sequentially, traverse from head using next pointer*/
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+public class Question1 {
+
+    // Insert node at the end
+    static Node insert(Node head, int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            return newNode;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+
+        return head;
+    }
+
+    // Traverse doubly linked list in forward direction
+    static void traverse(Node head) {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Node head = null;
+
+        for (int i = 0; i < n; i++) {
+            head = insert(head, sc.nextInt());
+        }
+
+        traverse(head);
+
+        sc.close();
+    }
+}
