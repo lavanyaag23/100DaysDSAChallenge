3bc500739d7af4f6d3172550f5ffee02da950aeb
Lavanya Agrawal <lavanya.kanak.agrawal@gmail.com>
2026-08-08 09:16:05 +0530

    Add binary tree construction from level order

---
+++ Day_43/Question1.java
@@ -0,0 +1,90 @@
+/* Problem Statement:
+Construct a Binary Tree from the given level-order traversal.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers (-1 represents NULL)
+
+Output Format:
+- Print inorder traversal of the constructed tree
+
+Example:
+Input:
+7
+1 2 3 4 5 -1 6
+
+Output:
+4 2 5 1 3 6*/
+
+import java.util.*;
+
+public class Question1 {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static Node buildTree(int[] arr) {
+        if (arr.length == 0 || arr[0] == -1) {
+            return null;
+        }
+
+        Node root = new Node(arr[0]);
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+
+        while (i < arr.length) {
+            Node current = queue.poll();
+
+            // Left child
+            if (i < arr.length && arr[i] != -1) {
+                current.left = new Node(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            // Right child
+            if (i < arr.length && arr[i] != -1) {
+                current.right = new Node(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Node root = buildTree(arr);
+
+        inorder(root);
+
+        sc.close();
+    }
+}
