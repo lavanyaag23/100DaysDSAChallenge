beefda34ff3216d306b0d1e74ebacc7738bb5288
Lavanya Agrawal <lavanya.kanak.agrawal@gmail.com>
2026-09-20 22:04:19 +0530

    Day 86: Find Integer Square Root

---
+++ Day_86/Question1.java
@@ -0,0 +1,63 @@
+/* Problem: Find the integer square root of a given non-negative integer using Binary Search. The integer square root of a number is defined as the greatest integer whose square is less than or equal to the given number.
+
+Input:
+- First line: an integer n
+
+Output:
+- Print the integer square root of n
+
+Examples:
+Input:
+64
+
+Output:
+8
+
+Input:
+20
+
+Output:
+4
+
+Input:
+1
+
+Output:
+1
+
+Explanation:
+Binary Search is applied on the range 0 to n. 
+At each step, the middle value is squared and compared with n. 
+If mid * mid equals n, mid is the square root.
+ If mid * mid is less than n, search continues in the right half; 
+ otherwise, in the left half. The final answer is the largest integer whose square does not exceed n */
+
+ import java.util.*;
+
+public class Question1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        long low = 0;
+        long high = n;
+        long answer = 0;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+
+            if (mid <= n / mid || mid == 0) {
+                answer = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+
+        sc.close();
+    }
+}
